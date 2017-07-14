58de31dd4e47a872b691198324ab84eb909e0c62
AwsIdris <awsidris@gmail.com>
2017-07-14 23:11:17 +0200

    Create Main Components.

---
+++ name-generator/src/components/GeneratorForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './GeneratorForm.css'
+export default class GeneratorForm extends React.Component {
+
+	render() {
+		return (
+			<div className="GeneratorForm">
+				FORM
+			</div>
+		)
+	}
+}
---
+++ name-generator/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Header.css'
+export default function Header() {
+	return(
+		 <div className="Header"> 
+			Company<br/>Name<br/>Generator 
+		 </div>
+	)
+}
